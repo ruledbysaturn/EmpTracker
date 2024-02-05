81c2ae6342dc510cb8a93ac20306af054895e5db
Ruminate <inramiland@gmail.com>
2024-02-05 17:49:33 -0500

    updated some typos

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,22 @@
-CREATE TABLE department (
+CREATE TABLE IF NOT EXISTS department (
     id INT PRIMARY KEY,
     name VARCHAR(30)
 );
 
-CREATE TABLE role (
+CREATE TABLE IF NOT EXISTS role (
     id INT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE IF NOT EXISTS employee (
     id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (manager_id) REFERENCES employee(id) 
 );
